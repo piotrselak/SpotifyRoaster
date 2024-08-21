8da4188c8c9fa48046c2692f1ff10f4b6708dd47
piotrselak <piotr.selak.it@gmail.com>
2024-08-21 18:18:03 +0200

    Experimenting with DRF

--- roaster/urls.py
+++ roaster/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from roaster import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('roast', views.RoastOperations.as_view(), name='roast'),
 ]
--- spotify_roaster/settings.py
+++ spotify_roaster/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "roaster.apps.RoasterConfig"
+    'roaster.apps.RoasterConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
