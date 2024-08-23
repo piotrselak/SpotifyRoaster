45ac434e01dd20f9c57fcef3953dd59c862c129e
piotrselak <piotr.selak.it@gmail.com>
2024-08-23 14:45:48 +0200

    Working prototype

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('generate', views.APIView.as_view(), name='generator'),
+    path('generate', views.RoastGenerator.as_view(), name='generator'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,7 +4,8 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.models import SpotifyUser
+from api.models import SpotifyUser, Track
+from common.openai import generate_roast
 from common.spotify import get_user_profile
 
 
@@ -17,9 +18,26 @@ class RoastGenerator(APIView):
         if not spotify_token:
             return Response("Token missing", status=status.HTTP_401_UNAUTHORIZED)
 
+        spotify_token = spotify_token.split('Bearer ')[1]
+
         user_profile = get_user_profile(spotify_token) # TODO ERROR HANDLING
         user = get_object_or_404(SpotifyUser, pk=user_profile.id)
 
         # TODO what if user logged second time after a few months so the artists would differ
         artists = user.artists.all()
-        tracks = user.artists.all()
+        tracks = user.tracks.all()
+
+        parsed_artists = map(lambda artist: artist.name, artists)
+
+        parsed_tracks = list(map(
+            lambda track: f"{track.name} by {', '.join(artist.name for artist in track.artists.all())}",
+            tracks
+        ))
+
+        prompt = ("Roast this spotify user based on this data. "
+                  f"Top user artists: {",".join(parsed_artists)}. "
+                  f"Top tracks: {",".join(parsed_tracks)}")
+
+        roast = generate_roast("You are a sassy, sarcastic roast comedian.", prompt)
+
+        return Response({"roast": roast.content})
